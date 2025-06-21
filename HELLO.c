190f1f4f834b5eaf3525af2c8c8f3c1cc8b88f27
AB-Ohi <askohi63907@gmail.com>
2025-06-21 17:10:08 +0600

    first commit

---
+++ HELLO.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int number = 30;
+    char star = '#';
+    int age = 22;
+    float pi = 3.14;
+    // case sensitive
+    int a = 40;
+    int A = 50;
+
+    // 1st character is alphabet or '_'
+
+    int _age = 25;
+    // {wrong}
+    // int -age = 25;
+    // int 1age = 25
+
+    // no comma / blank space
+    int final_price = 10000;
+    // int final price = 2000
+
+    
+    return 0;
+}
+
---
+++ dataType.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+// int main()
+// {
+//     int age = 22;
+//     float pi = 3.14;
+//     char hashtag = '#';
+     
+//     return 0;
+// }
+
+// int main (){
+//     printf("hello c \n");
+//     printf("hello c \n");
+//     printf("hello c \n");
+//     printf("hello c \n");
+//     return 0;
+// }
+
+// int main (){
+//     char my_name = '*';
+//     printf("my name is %c \n", my_name);
+//     return 0;
+// }
+
+int main (){
+    int age;
+    printf("enter age");
+    scanf("%d", &age);
+    printf("age is : %d", age);
+}
