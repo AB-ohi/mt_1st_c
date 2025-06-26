28e90e65558d1f85bb852621233c2b7cac7f3a7c
AB-Ohi <askohi63907@gmail.com>
2025-06-26 22:43:32 +0600

    added

--- question.c
+++ question.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+// #include<math.h>
 // int main()
 // {
 //     float side;
@@ -11,11 +11,33 @@
 // }
 
 
+// int main (){
+//     float radius;
+//     printf("enter radius : ");
+//     scanf("%f", &radius);
+//     float pi = 3.14;
+//     printf("area of circle is : %f", pi*radius*radius);
+//     return 0;
+// }
+
+// int main (){
+//     int x;
+//     printf("enter a number: ");
+//     scanf("%d", &x);
+//     printf("%d", x%2 == 0);
+
+
+//     return 0;
+// }
+
 int main (){
-    float radius;
-    printf("enter radius : ");
-    scanf("%f", &radius);
-    float pi = 3.14;
-    printf("area of circle is : %f", pi*radius*radius);
+    // int isMonday = 1 ;
+    // int isRaining = 0;
+    // printf("%d", isMonday && isRaining );
+
+    int x;
+    printf("enter number: ");
+    scanf("%f", &x);
+    printf("%d \n", x>9 && x<100);
     return 0;
 }
